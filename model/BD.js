84688b8461a976a954f3f6ca047951c1d5c95e8e
Daltsz <gabrieldadalto@hotmail.com>
2020-10-28 21:18:13 -0300

    Servidor da aplicação do projeto final

---
+++ model/BD.js
@@ -0,0 +1,43 @@
+const MongoClient = require("mongodb").MongoClient;
+
+
+module.exports =  class User{
+
+    static async find(email){
+        
+        const conn = await MongoClient.connect("mongodb://127.0.0.1:27017/"),
+            db = conn.db("ProjFinal");
+        if(email)
+            return await db.collection("Users").find({email: email}).toArray();    
+
+    }
+
+
+    static async findLogin(value){
+        const conn = await MongoClient.connect("mongodb://127.0.0.1:27017/"),
+            db = conn.db("ProjFinal");
+        if(value){
+            return await db.collection("Users").find({email: value.email, password: value.password}).toArray();    
+        }
+
+    }
+
+
+
+    static async findWord(word){
+        const conn = await MongoClient.connect("mongodb://127.0.0.1:27017/"),
+            db = conn.db("ProjFinal");
+        if(word){
+            return await db.collection("words").find({word: word}).toArray();    
+        }
+
+    }
+
+
+    static async insert(user){
+        const conn = await MongoClient.connect("mongodb://127.0.0.1:27017/"),
+            db = conn.db("ProjFinal");
+        db.collection('Users').insertOne({first_Name: user.first_Name, last_name: user.last_name, email: user.email, password: user.password});
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,149 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const dbConect = require("./model/BD");
+
+
+let xinxila  = '';
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+
+app.use(session({
+
+    secret: 'config.session.secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+
+
+
+}));
+
+
+app.get("/", async (req, res, ) =>{
+
+    req.session.login = xinxila;
+
+    if(req.session && req.session.login){
+
+        
+        res.json(req.session.id);
+
+    }else{
+
+        res.json('false');
+
+    }
+
+  
+
+});
+
+
+
+
+
+
+app.post("/api/register", async (req, res) =>{
+
+    const user = req.body;
+    const email = req.body.email;
+    register =  await dbConect.find(email);
+
+    console.log(register)
+
+    if(register[0] != null){
+        console.log(register)
+        res.json(-1)
+
+    }else{
+        
+        dbConect.insert(user);
+        res.json(1);
+
+    }
+    
+
+
+
+});
+
+
+
+
+app.post("/api/query", async (req, res) =>{
+
+    const word = req.body.word;
+    
+    wordFelling =  await dbConect.findWord(word);
+
+    if(typeof wordFelling[0] === 'undefined'){
+
+        res.json(`${word} - Neutral`);
+
+    }else{
+
+
+        res.json(`${wordFelling[0].word} - ${wordFelling[0].felling} `);
+
+
+
+    }
+
+});
+
+
+
+
+app.post("/api/login", async (req, res) =>{
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const value = req.body;
+
+    login =  await dbConect.findLogin(value);
+
+
+    if(typeof login[0] === 'undefined'){
+
+        res.json(-1)
+
+
+    }else{
+
+        if(login[0].email == email && login[0].password == password){
+
+
+            xinxila = email
+    
+            res.json(1);
+    
+        }
+
+
+
+    }
+        
+
+});
+
+
+
+
+
+
+
+
+app.listen(10000, () => {
+
+    console.log("Servidor Esta rodando na Porta 10000");
+
+
+
+});
